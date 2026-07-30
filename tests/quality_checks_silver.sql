c836665ff270d01a8e859224e3174638199112ae
molibozhko <molibozhko.elizabeth@gmail.com>
2026-07-30 18:35:47 +0300

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -19,7 +19,11 @@ Usage Notes:
 ===================================================================
 ﻿Checking 'silver,crm_cust_info'
 ===================================================================
-﻿﻿-- Check for NULLs or Duplicates in Primary Key
+﻿﻿SELECT 
+*
+FROM bronze.crm_cust_info;
+	
+-- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results
 SELECT 
   cst_id,
@@ -28,6 +32,11 @@ FROM bronze.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
+SELECT
+*
+FROM bronze.crm_cust_info
+WHERE cst_id = 29466;
+
 -- Check for particular cst_id = 29466; duplicates (checking how func ROW_NUMBER() is working)
 SELECT
 *,
@@ -35,12 +44,24 @@ ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_las
 FROM bronze.crm_cust_info
 WHERE cst_id = 29466;
 
+SELECT
+*,
+ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info;
+
 -- Choosing all of duplicates (need a subquerry bcz clause in Where is erlier than columns is Select clause)
 SELECT * FROM 
   (SELECT *,
   ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
   FROM bronze.crm_cust_info
 )t WHERE flag_last != 1;
+
+-- vice versa 
+SELECT * FROM 
+  (SELECT *,
+  ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+  FROM bronze.crm_cust_info
+)t WHERE flag_last = 1;
 
 -- Choosing unique row with cst_id = 29466
 SELECT
@@ -59,7 +80,18 @@ SELECT
 FROM bronze.crm_cust_info
 WHERE cst_id IS NOT NULL
 
--- Check for unwanted Spaces
+-- Check for unwanted Spaces (If the original value is not equal to the same value after trimming, it means there are spaces)
+-- Expectation: No Results
+SELECT 
+  cst_key
+FROM bronze.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
+	
+SELECT 
+  cst_firstname
+FROM bronze.crm_cust_info;
+
+-- Check for unwanted Spaces 
 -- Expectation: No Results
 SELECT 
   cst_firstname
@@ -85,8 +117,8 @@ SELECT
 	  WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
 	  ELSE 'n/a'
     END cst_marital_status, -- Normalize marital status velues to readable format
-  CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-	  WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+  CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female' -- Apply Upper() just in case mixed-case velues appear later in your column
+	  WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' -- Apply TRIM() just in case spaces appear later in your column
 	  ELSE 'n/a'
     END cst_gndr, -- Normalize gender values to redable format
 FROM bronze.crm_cust_info
@@ -115,8 +147,55 @@ FROM (
 )t WHERE flag_last = 1; -- Select the most recent record per customer
 
 ===================================================================
+-- Qality check of the Silver Table
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+  cst_id,
+  COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for unwanted Spaces 
+-- Expectation: No Results
+SELECT 
+  cst_key
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
+
+-- Check for unwanted Spaces 
+-- Expectation: No Results
+SELECT 
+  cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+-- Check for unwanted Spaces 
+-- Expectation: No Results
+SELECT 
+  cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+SELECT * FROM silver.crm_cust_info
+
+===================================================================
 ﻿Checking 'silver,crm_prd_info'
 ===================================================================
+SELECT
+	prd_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info;
 
 -- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results  
@@ -126,6 +205,78 @@ SELECT
 FROM bronze.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Filters out unmatched data after applying transformation
+SELECT
+	prd_id,
+	prd_key,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info;
+
+SELECT DISTINCT id FROM bronze.erp_px_cat_g1ve; -- We should double check this table, bcz this table could be joined to prd_info
+-- As we see id FROM bronze.erp_px_cat_g1ve includs '_' instead of '-'
+	
+SELECT
+	prd_id,
+	prd_key,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info
+WHERE REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') NOT IN -- We try to find cat_id which is not in erp_px_cat_g1ve
+(SELECT DISTINCT id FROM bronze.erp_px_cat_g1ve);
+-- We don't have only one cat_id which is not included in bronze.erp_px_cat_g1ve, check this cat_id in bronze.erp_px_cat_g1ve:
+
+SELECT DISTINCT id FROM bronze.erp_px_cat_g1ve;  -- we definetly don't have this one cat_id in bronze.erp_px_cat_g1ve, that's fine
+
+-- Filters out unmatched data after applying transformation
+SELECT
+	prd_id,
+	prd_key,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info;
+
+SELECT sls_prd_key FROM bronze.crm_sales_details; -- As we see prd_key looks like  in bronze.crm_prd_info, but we should check:
+
+SELECT
+	prd_id,
+	prd_key,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info
+WHERE SUBSTRING(prd_key, 7, LEN(prd_key)) NOT IN 
+(SELECT  sls_prd_key FROM bronze.crm_sales_details); -- As we see a lot of prd_key from bronze.crm_prd_info which (products don't have any orders)
+-- are not in bronze.crm_sales_details - we can check the exact prd_key(which will be cutted on several symbols)
+SELECT sls_prd_key FROM bronze.crm_sales_details WHERE sls_prd_key LIKE 'FK-16%';
+SELECT sls_prd_key FROM bronze.crm_sales_details WHERE sls_prd_key LIKE 'FK-1%';
+SELECT sls_prd_key FROM bronze.crm_sales_details WHERE sls_prd_key LIKE 'FK%';
+
+-- So we don't have such prd_key in bronze.crm_sales_detail, but
+-- we still have a lot of prd_key which could be joined with sls_prd_key in bronze.crm_sales_details:
+SELECT
+*
+FROM bronze.crm_prd_info
+WHERE SUBSTRING(prd_key, 7, LEN(prd_key)) IN 
+(SELECT sls_prd_key FROM bronze.crm_sales_details);
 
 -- Check for unwanted Spaces
 -- Expectation: No Results
@@ -144,12 +295,29 @@ FROM bronze.crm_prd_info;
 SELECT 
   prd_cost
 FROM bronze.crm_prd_info
-WHERE prd_cost < 0 OR prd_cost IS NULL;
+WHERE prd_cost < 0 OR prd_cost IS NULL; 
 
--- Check for Invalid Date Orders
+-- Check for Invalid Date Orders, end_date must be earlier than start_date
 SELECT *
 FROM bronze.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
+
+-- For complex transformations in SQL better to narrow it down to a specific exzmple and brainstorm multiple
+-- solution approaches
+-- Solution # 1: Switxh End Date and Start Date, If no overlapping
+-- Solition # 2: Derive the End Date from the Start Date (End Date = Start Date of the Next Record), even better to substruct 1 day
+-- which exclude overlapping
+
+-- It's better to check on exzct rows with issues dates and If the choosen logic is working
+SELECT
+	prd_id,
+	prd_key,
+	prd_nm,
+	prd_start_dt,
+	prd_end_dt,
+	LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_test
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE_HL-U509-R', 'AC-HE-HL-U509')
 
 -- Final querry 
 SELECT
@@ -169,6 +337,58 @@ CAST (prd_start_dt AS DATE) AS prd_start_dt,
 CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info
 ORDER BY prd_id;
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL 
+	DROP TABLE silver.crm_prd_info;
+﻿CREATE TABLE silver.crm_prd_info (
+	prd_id INT, 
+	cat_id NVARCHAR(50), 
+	prd_key NARCHAR(50), 
+	prd_nm NVARCHAR(50), 
+	pra_cost INT, 
+	prd_line NVARCHAR(50), 
+	prd_start_dt DATE, -- changing the format from DATETIME to DATE
+	prd. _end_dt DATE, 
+	dwh_create_date DATETIME DEFAULT GETDATE ();
+
+===================================================================
+-- Qality check of the Silver Table
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results  
+SELECT 
+  prd_id,
+  COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted Spaces
+-- Expectation: No Results
+SELECT 
+  prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No Results
+SELECT 
+  prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL; 
+
+-- Data Standartization & Consistency
+SELECT DISTINCT 
+  prd_line
+FROM silver.crm_prd_info;
+
+-- Check for Invalid Date Orders, end_date must be earlier than start_date
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+SELECT *
+FROM silver.crm_prd_info;
 
 ===================================================================
 ﻿Checking 'silver,crm_sales_details'
@@ -241,6 +461,7 @@ SELECT
 FROM bronze.crm_sales_details
 WHERE sls_order_dt > 20500101;
 
+-- Check for outliers by validating the boundaries of the date range
 SELECT
   NULLIF(sls_order_dt,0) sls_order_dt
 FROM bronze.crm_sales_details
@@ -300,13 +521,14 @@ OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <=0
 ORDER BY sls_sales, sls_quantity, sls_price;
 -- According to what we have (negative values, zeros and nulls) - need to talk with someone from the bisness 
 -- or source system and discuss, two solution:
-	-- data issues will be fixed direct in source system
-	-- data issues has to be fixed in data warehouse 
+-- data issues will be fixed direct in source system
+-- data issues has to be fixed in data warehouse 
 
 -- We choosing all of broken data and following the rules:
 -- If Sales is negative, zero, or null, derive it using Quantity and Price.
 -- If Price is zero, or null, calculate it using Sales and Quantity. 
--- If Price is negative, convert it to a positive value
+-- If Price is negative, convert it to a positive value 
+-- In result should be cleaned and correct data
 SELECT DISTINCT
 sls_sales as old_sls_sales,
 sls_quantity,
@@ -342,15 +564,16 @@ END AS sls_due_dt,
 CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
 	THEN sls_quantity * ABS(sls_price)
 	ELSE sls_sales
-END AS sls_sales,
+END AS sls_sales, -- Recalculate sales if origal values is mising or incorrect
 sls_quantity,
 CASE WHEN sls_price IS NULL OR sls_price <= 0
 	THEN sls_sales / NULLIF(sls_quantity, 0)
-	ELSE sls_price
+	ELSE sls_price -- Derive price if original value is missing or incorrect 
 END AS sls_price
 FROM bronze.crm_sales_details;
 
--- Because of changing format of dates (from integer to Date) - we should check the DDL 
+-- Because of changing format of dates (from integer to Date) - we should check the DDL and change the format
+-- of the table before inserting data in silver table
 IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
 DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details ( 
@@ -364,6 +587,35 @@ CREATE TABLE silver.crm_sales_details (
   sls_quantity INT, 
   sls_price INT
 );
+
+===================================================================
+-- Qality check of the Silver Table
+
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt;
+
+-- Check for Invalid Date Orders	
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt;
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+SELECT DISTINCT
+	sls_sales,
+	sls_quantity,
+	sls_price,
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <=0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+SELECT *
+FROM silver.crm_sales_details
+
 ===================================================================
 ﻿Checking 'silver,erp_cust_az12'
 ===================================================================
