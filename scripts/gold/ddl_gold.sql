bcd682e819b0d5c9a1551a3e881140ebcc55e553
molibozhko <molibozhko.elizabeth@gmail.com>
2026-08-07 13:18:53 +0300

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -14,7 +14,7 @@ Usage:
 -- Create dimension: gold.dim_customers
 -- ===================================================================
 IF OBJECT_ID ('gold.dim_customers', 'V') IS NOT NULL
-DROP VIEW gold.dim_customers;
+	DROP VIEW gold.dim_customers;
 GO 
 
 CREATE VIEW gold.dim_customers AS
@@ -33,16 +33,16 @@ SELECT
 	ci.cst_create_date AS create_date
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
-ON ci.cst_key = ca.cid
+	ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
-ON ci.cst_key = la.cid;
+	ON ci.cst_key = la.cid;
 GO
 
 -- ===================================================================
 -- Create dimension: gold.dim_products
 -- ===================================================================
 IF OBJECT_ID ('gold.dim_products', 'V') IS NOT NULL
-DROP VIEW gold.dim_products;
+	DROP VIEW gold.dim_products;
 GO 
 
 CREATE VIEW gold.dim_products AS
@@ -60,7 +60,7 @@ SELECT
 	pn.prd_start_dt AS start_date
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
-ON pn.cat_id = pc.id
+	ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL;
 GO
 
@@ -68,23 +68,23 @@ GO
 -- Create fact: gold.fact_sales
 -- ===================================================================
 IF OBJECT_ID ('gold.fact_sales', 'V') IS NOT NULL
-DROP VIEW gold.fact_sales;
+	DROP VIEW gold.fact_sales;
 GO 
 
 CREATE VIEW gold.fact_sales AS
 SELECT
-sd.sls_ord_num AS order_number,
-pr.product_key, 
-cu.customer_key,
-sd.sls_order_dt AS order_date,
-sd.sls_ship_dt AS shipping_date,
-sd.sls_due_dt AS due_date,
-sd.sls_sales AS sales_amount,
-sd.sls_quantity AS quantity,
-sd.sls_price AS price
+	sd.sls_ord_num AS order_number,
+	pr.product_key AS product_key, 
+	cu.customer_key AS customer_key,
+	sd.sls_order_dt AS order_date,
+	sd.sls_ship_dt AS shipping_date,
+	sd.sls_due_dt AS due_date,
+	sd.sls_sales AS sales_amount,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
-  ON sd.sls_prd_key = pr.product_number
+	ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
-ON sd.sls_cust_id = cu.customer_id;
+	ON sd.sls_cust_id = cu.customer_id;
 GO
